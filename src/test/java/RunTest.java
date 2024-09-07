49238a5d6d907f07117bb52a0b17eefab77a3bb2
Simakin_Evgeniy <simakin174@mail.ru>
2024-09-07 14:44:54 +0300

    Написал тесты по заданию c изменениями

--- src/test/java/RunTest.java
+++ src/test/java/RunTest.java
@@ -1,4 +1,25 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class RunTest {
     //its RunTest origin
 
+
+    @BeforeEach
+    public void setup() {
+        System.out.println("Setting upup before each test");
+    }
+
+    @AfterEach
+    public void teardown() {
+        System.out.println("Tearing down after each testing");
+    }
+
+    @Test
+    public void test1() {
+        System.out.println("Running test 1");
+        Assertions.assertEquals("AW", "AW", "Test 1 failed");
+    }
 }
